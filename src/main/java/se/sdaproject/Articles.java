ffc30c0ef782ebd16e29d762abea8fad19c96c5a
ismail celik <ism-celik@hotmail.com>
2021-04-13 15:18:11 +0200

    Initial commit

---
+++ src/main/java/se/sdaproject/Articles.java
@@ -0,0 +1,92 @@
+package se.sdaproject;
+
+
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+public class Articles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String body;
+
+    private String authorName;
+
+    @OneToMany(mappedBy = "commentedArticle")
+    private List<ArticlesComments> articleCommentsList;
+
+    @ManyToMany(mappedBy = "articlesList")
+    @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "name")
+    @JsonIdentityReference(alwaysAsId = true)
+    private Set<Topics> topicsList;
+
+    public Articles(){}
+
+    public Articles(String title, String body, String authorName) {
+        this.title = title;
+        this.body = body;
+        this.authorName = authorName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Set<Topics> getTopicsList() {
+        return topicsList;
+    }
+
+    public void setTopicsList(Set<Topics> topicsList) {
+        this.topicsList = topicsList;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public void setAuthorName(String authorName) {
+        this.authorName = authorName;
+    }
+
+    public void updateArticle(Articles updatedArticle){
+        this.title = updatedArticle.title;
+        this.authorName=updatedArticle.authorName;
+        this.body=updatedArticle.body;
+    }
+
+    public List<ArticlesComments> getArticleCommentsList() {
+        return articleCommentsList;
+    }
+
+
+    public void setArticleCommentsList(List<ArticlesComments> articleCommentsList) {
+        this.articleCommentsList = articleCommentsList;
+    }
+}
---
+++ src/main/java/se/sdaproject/ArticlesCommentsController.java
@@ -0,0 +1,60 @@
+package se.sdaproject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ArticlesCommentsController {
+    ArticlesCommentsRepository articlesCommentsRepository;
+    ArticlesRepository articlesRepository;
+
+    @Autowired
+    public ArticlesCommentsController(ArticlesCommentsRepository articlesCommentsRepository, ArticlesRepository articlesRepository){
+        this.articlesRepository = articlesRepository;
+        this.articlesCommentsRepository = articlesCommentsRepository;
+    }
+
+
+
+    @GetMapping("/articles/{articleId}/comments")
+    public List<ArticlesComments> getArticlesCommentsList(@PathVariable Long articleId){
+      Articles article = articlesRepository.findById(articleId).orElseThrow(ItemNotFoundException::new);
+      return article.getArticleCommentsList();
+    }
+
+    //Create a new comment:
+    @PostMapping("/articles/{articleId}/comments")
+    public ResponseEntity<ArticlesComments> createComment(@PathVariable Long articleId,@Validated @RequestBody ArticlesComments articlesComments){
+        Articles article = articlesRepository.findById(articleId).orElseThrow(ItemNotFoundException::new);
+        articlesComments.setCommentedArticle(article);
+        articlesCommentsRepository.save(articlesComments);
+        return ResponseEntity.status(HttpStatus.CREATED).body(articlesComments);
+
+    }
+    @DeleteMapping("/comments/{id}")
+    public String deleteCommentById(@PathVariable Long id){
+        articlesCommentsRepository.deleteById(id);
+        return "Comment deleted successfully";
+    }
+
+    @PutMapping("/comments/{id}")
+    public ResponseEntity<ArticlesComments> updateComment(@PathVariable Long id,@Validated @RequestBody ArticlesComments commentParams){
+        ArticlesComments existingComment = articlesCommentsRepository.findById(id).orElseThrow(ItemNotFoundException::new);
+        commentParams.setId(id);
+        articlesCommentsRepository.save(commentParams);
+        return ResponseEntity.ok(commentParams);
+    }
+
+
+    @GetMapping(value = "/comments", params = {"authorName"})
+    public ResponseEntity<List<ArticlesComments>> viewAllCommentsByAuthor(@RequestParam String authorName) {
+        return ResponseEntity.ok(articlesCommentsRepository.findByAuthorName(authorName));
+    }
+
+
+}
